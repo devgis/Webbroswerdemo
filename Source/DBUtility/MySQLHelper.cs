2bdf4f591967277ee81d93b76c620d3c4cab7d1d
devgis <devgis@163.com>
2022-05-17 15:07:04 +0800

    Public
    Public

---
+++ Source/DBUtility/MySQLHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace DBUtility
+{
+    public class MySQLHelper
+    {
+        private MySqlConnection StyleConnection;
+
+        #region 构造方法
+        private MySQLHelper()
+        {
+            #region 初始化连接信息
+            string strConStr = System.Configuration.ConfigurationManager.AppSettings["MySQL"].ToString();
+            StyleConnection = new MySqlConnection(strConStr);
+            #endregion
+        }
+        #endregion
+
+        #region 单例
+        private static MySQLHelper _instance = null;
+
+        /// <summary>
+        /// PGHelper的实例
+        /// </summary>
+        public static MySQLHelper Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new MySQLHelper();
+                }
+                return _instance;
+            }
+        }
+        #endregion
+
+        #region 公有方法
+        /// <summary>
+        /// 从型号基础库获取数据
+        /// </summary>
+        /// <param name="SQL">查询的SQL语句</param>
+        /// <returns>查询的结果</returns>
+        public DataTable GetDataTable(String SQL)
+        {
+            MySqlCommand cmd = new MySqlCommand(SQL, StyleConnection);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        /// <summary>
+        /// 执行单个SQL
+        /// </summary>
+        /// <param name="Sql">需要执行的SQL</param>
+        /// <returns>执行结果</returns>
+        public bool ExecuteSql(String Sql)
+        {
+            StyleConnection.Open();
+            try
+            {
+                MySqlCommand oc = new MySqlCommand(Sql, StyleConnection);
+                if (oc.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+            }
+            finally
+            {
+                StyleConnection.Close();
+            }
+            return false;
+        }
+        #endregion
+
+    }
+}
---
+++ Source/TestAPP/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace TestAPP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Url = new Uri(Path.Combine(Application.StartupPath, "index.html"));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable dt = DBUtility.MySQLHelper.Instance.GetDataTable("select * from t_test");
+            string result = JsonConvert.SerializeObject(dt, new DataTableConverter());
+
+            // 调用JavaScript的messageBox方法，并传入参数
+            object[] objects = new object[1];
+            objects[0] = result;
+            this.webBrowser1.Document.InvokeScript("senddata", objects);
+        }
+    }
+}
